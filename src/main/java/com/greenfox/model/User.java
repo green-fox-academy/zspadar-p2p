ada0dff69f5b93458ecf9c9708ed5e88b24c49ff
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-17 17:13:54 +0200

    init(User class):as an entity

---
+++ src/main/java/com/greenfox/model/User.java
@@ -0,0 +1,41 @@
+package com.greenfox.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by zsuzsanna.padar on 2017. 05. 17..
+ */
+@Entity
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  int id;
+
+  String userName;
+
+  public User(String userName) {
+    this.userName = userName;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+  
+}
