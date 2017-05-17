42ee9ce0a6071094cff00726c3946818edb598f0
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-17 15:24:27 +0200

    init(Log java class)

---
+++ src/main/java/com/greenfox/model/Log.java
@@ -0,0 +1,38 @@
+package com.greenfox.model;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by zsuzsanna.padar on 2017. 05. 17..
+ */
+public class Log {
+  private String path;
+  private String method;
+  private Timestamp dateAndTime;
+  private String logLevel;
+  private String requestData;
+
+  public Log() {
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public String getMethod() {
+    return method;
+  }
+
+  public Timestamp getDateAndTime() {
+    return dateAndTime;
+  }
+
+  public String getLogLevel() {
+    return logLevel;
+  }
+
+  public String getRequestData() {
+    return requestData;
+  }
+  
+}
