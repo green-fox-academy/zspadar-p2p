a24c905edfe499a7f68f5211fc58401ca51f4d3f
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-18 17:02:46 +0200

    init(Message java class)

---
+++ src/main/java/com/greenfox/model/Message.java
@@ -0,0 +1,51 @@
+package com.greenfox.model;
+
+import java.sql.Timestamp;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+/**
+ * Created by zsuzsanna.padar on 2017. 05. 18..
+ */
+@Entity
+@Table(name = "Messages")
+public class Message {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  long id;
+
+  String message;
+  String userName;
+  Timestamp timestamp;
+
+  public Message() {
+  }
+
+  public Message(String userName, String message) {
+    this.id = 1000000 + ((long)(Math.random() * 9999999));
+    this.message = message;
+    this.userName = userName;
+    this.timestamp = new Timestamp(System.currentTimeMillis());
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public Timestamp getTimestamp() {
+    return timestamp;
+  }
+}
