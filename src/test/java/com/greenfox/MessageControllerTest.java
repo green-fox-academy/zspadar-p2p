a0aba1a8b4760f7cce6797112bd5c38b29bc2798
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-23 15:33:39 +0200

    add(CI test)

---
+++ src/test/java/com/greenfox/MessageControllerTest.java
@@ -0,0 +1,76 @@
+package com.greenfox;
+
+import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import com.greenfox.repository.MessageRepository;
+import com.greenfox.repository.UserRepository;
+import java.nio.charset.Charset;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Created by zsuzsanna.padar on 2017. 05. 23..
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = P2pchatApplication.class)
+@WebAppConfiguration
+@EnableWebMvc
+public class MessageControllerTest {
+
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+      MediaType.APPLICATION_JSON.getSubtype(),
+      Charset.forName("utf8"));
+
+  private MockMvc mockMvc;
+
+  @Autowired
+  UserRepository userRepository;
+
+  @Autowired
+  MessageRepository messageRepository;
+
+  @Autowired
+  WebApplicationContext webApplicationContext;
+
+
+  @Before
+  public void setup() throws Exception {
+    this.mockMvc = webAppContextSetup(webApplicationContext).build();
+  }
+
+  @Test
+  public void testReceivedJson() throws Exception {
+    mockMvc.perform(post("/api/message/receive")
+    .contentType(MediaType.APPLICATION_JSON)
+    .content("{\n"
+        + "  \"message\": {\n"
+        + "    \"id\": 7655482,\n"
+        + "    \"username\": \"EggDice\",\n"
+        + "    \"text\": \"How you doin'?\",\n"
+        + "    \"timestamp\": 1322018752992\n"
+        + "  },\n"
+        + "  \"client\": {\n"
+        + "    \"id\": \"EggDice\"\n"
+        + "  }\n"
+        + "}"))
+    .andExpect(status().isOk())
+    .andExpect(content().contentType(contentType))
+    .andExpect(jsonPath("$.status").value("ok"));
+  }
+
+
+}
